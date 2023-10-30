da26b0833b6f8aec6f3a52904e1e460347797f01
willcutrim <willyam894@gmail.com>
2023-10-30 11:52:33 -0300

    TEST: Criado teste de validação de email

--- produtos/tests.py
+++ produtos/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from .models import Produto, Fornecedor
 
-# Create your tests here.
+class FornecedorTestCase(TestCase):
+    
+    def set_character(self, email):
+        if '@' in email and '.com' in email:
+            return True
+        else:
+            return False
+
+    def teste_validation_email_fornecedor(self):
+        fornecedor = Fornecedor.objects.create(nome_fornecedor='Casos Bahio', contato=99991919906, email='willyam894@gmail.com')
+        false_or_true = self.set_character(fornecedor.email)
+
+        self.assertEqual(false_or_true, True)
+        
+        
